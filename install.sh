85426187698d29c015a77424dd111406898c6994
Mr. SAGE <thehackingsage@users.noreply.github.com>
2018-03-19 15:50:56 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 clear
-echo "
-          _   _   ___  _____  _   _____________ _____ _   _ _____  ___   _   _ 
-         | | | | / _ \/  __ \| | / /_   _| ___ \  _  | \ | |_   _|/ _ \ | \ | |
-         | |_| |/ /_\ \ /  \/| |/ /  | | | |_/ / | | |  \| | | | / /_\ \|  \| |
-         |  _  ||  _  | |    |    \  | | |    /| | | | . ` | | | |  _  || . ` |
-         | | | || | | | \__/\| |\  \ | | | |\ \\ \_/ / |\  |_| |_| | | || |\  |
-         \_| |_/\_| |_/\____/\_| \_/ \_/ \_| \_|\___/\_| \_/\___/\_| |_/\_| \_/
-                                                                      
-                                                ~ Tools for Hacking by Mr. SAGE
-Twitter.com/thehackingsage | Instagram.com/thehackingsage | Github.com/thehackingsage
-";
+
+BLACK='\e[30m'
+RED='\e[31m'
+GREEN='\e[32m'
+YELLOW='\e[33m'
+BLUE='\e[34m'
+PURPLE='\e[35m'
+CYAN='\e[36m'
+WHITE='\e[37m'
+NC='\e[0m'
+echo""
+echo -e "${RED} _   _   ___  _____  _   _____________ _____ _   _ _____  ___   _   _  "
+echo " | | | | / _ \/  __ \| | / /_   _| ___ \  _  | \ | |_   _|/ _ \ | \ | | "
+echo " | |_| |/ /_\ \ /  \/| |/ /  | | | |_/ / | | |  \| | | | / /_\ \|  \| | "
+echo " |  _  ||  _  | |    |    \  | | |    /| | | | . ` | | | |  _  || . ` | "
+echo " | | | || | | | \__/\| |\  \ | | | |\ \\ \_/ / |\  |_| |_| | | || |\  | "
+echo " \_| |_/\_| |_/\____/\_| \_/ \_/ \_| \_|\___/\_| \_/\___/\_| |_/\_| \_/ "
+echo -e "${NC}"
+echo -e "${RED}                               ~ Tools for Hacking by Mr. SAGE ${NC}"
+echo ""
+echo -e "${YELLOW} Twitter.com/thehackingsage | Instagram.com/thehackingsage | Github.com/thehackingsage ${NC} "
+echo ""
+echo "---------------------------------------------------------------------------------"
+echo ""
+echo -e "${CYAN}[>] Press ENTER to Install, CTRL+C to Abort.${NC}"
+read INPUT
+echo ""
 
 if [ "$PREFIX" = "/data/data/com.termux/files/usr" ]; then
     INSTALL_DIR="$PREFIX/usr/share/doc/hacktronian"
