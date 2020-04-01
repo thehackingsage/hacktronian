66b20b67781739ee01938c6a3398f2f00c82bf27
programmingAthlete <luca.bonamino@hotmail.com>
2020-04-01 11:44:39 +0200

    Object oriented functionality

---
+++ Setup/setup.py
@@ -0,0 +1,113 @@
+
+intro = {
+    1 : 'Information Gathering',
+    2 : 'Password Attacks',
+    3 : 'Wireless Testing',
+    4 : 'Exploitation Tools',
+    5 : 'Sniffing & Spoofing',
+    6 : 'Web Hacking',
+    7 : 'Private Web Hacking',
+    8 : 'Post Exploitation',
+    0 : 'Install The HACKTRONIAN',
+    99 : 'Exit'}
+
+
+info = {
+    1 : 'Nmap',
+    2 : 'Setoolkit',
+    3 : 'Port Scanning',
+    4 : 'Host To IP',
+    5 : 'wordpress user',
+    6 : 'CMS scanner',
+    7 : 'XSStrike',
+    8 : 'Dork - Google Dorks Passive Vulnerability Auditor',
+    9 : "Scan A server's Users",
+    10 : "Crips",
+    98 : "Back",
+    99 : "Exit"
+}
+
+passwd = {
+    1 : 'Cupp',
+    2 : "Ncrack",
+    98 : 'Back',
+    99 : 'Exit'
+}
+
+wire = {
+    1 : "reaver",
+    2 : "pixiewps",
+    3 : "Bluetooth Honeypot GUI Framework",
+    4 : "Fluxion",
+    98 : "Back",
+    99 : "Exit"
+}
+
+exp = {
+    1 : "ATSCAN",
+    2 : "sqlmap",
+    3 : "Shellnoob",
+    4 : "commix",
+    5 : 'FTP Auto Bypass',
+    6 : "jboss-autopwn",
+    7 : "Blind SQL Automatic Injection And Exploit",
+    8 : "Bruteforce the Android Passcode given the hash and salt",
+    9 : "Joomla SQL injection Scanner",
+    98 : "Back",
+    99: "Exit"
+}
+
+sniff = {
+    1 : "Setoolkit",
+    2 : "SSLtrip",
+    3 : "pyPISHER",
+    4 : "SMTP Mailer",
+    98 : "Back",
+    99 : "Exit"
+}
+
+webhack = {
+    1 : 'Drupal Hacking',
+    2 : 'Inurlbr',
+    3 : 'Wordpress & Joomla Scanner',
+    4 : 'Gravity Form Scanner',
+    5 : 'File Upload Checker',
+    6 : 'Wordpress Exploit Scanner',
+    7 : 'Wordpress Plugins Scanner',
+    8 : 'Shell and Directory Finder',
+    9 : 'Joomla! 1.5 - 3.4.5 remote code execution',
+    10 : 'Vbulletin 5.X remote code execution',
+    11 : 'BruteX - Automatically brute force all services running on a target',
+    12 : 'Arachni - Web Application Security Scanner Framework',
+    98 : 'Back',
+    99 : 'Exit'
+}
+
+postexp = {
+    1 : 'Shell Chcker',
+    2 : 'POET',
+    3 : 'Phishing Framework',
+    98 : 'Back',
+    99 : 'Exit'
+    }
+
+links = [
+    ('Information Gathering', 1),
+    ('Password Attacks', 2),
+    ('Wireless Testing', 3),
+    ('Exploitation Tools', 4),
+    ('Sniffing & Spoofing', 5),
+    ('Web Hacking', 6),
+    ('Post Exploitation', 8)
+    ]
+
+directories = [
+    intro,
+    info,
+    passwd,
+    wire,
+    exp,
+    sniff,
+    webhack,
+    postexp
+    ]
